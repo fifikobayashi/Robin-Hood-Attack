a17a4c45b798489166d422cff6f4b32045564a7d
Fiona Kobayashi <57852197+fifikobayashi@users.noreply.github.com>
2020-12-22 19:57:58 +1100

    Create IBalancerToken.sol

---
+++ IBalancerToken.sol
@@ -0,0 +1,9 @@
+interface IBalancerToken {
+    function balanceOf(address) external returns (uint);
+    function allowance(address, address) external returns (uint);
+    function approve(address, uint) external returns (bool);
+    function transfer(address, uint) external returns (bool);
+    function transferFrom(address, address, uint) external returns (bool);
+    function deposit() external payable;
+    function withdraw(uint) external;
+}
