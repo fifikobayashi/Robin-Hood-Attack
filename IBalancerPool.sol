155870c68e418ff6af97b3fb155cc7d2aa261dc6
Fiona Kobayashi <57852197+fifikobayashi@users.noreply.github.com>
2020-12-22 19:57:42 +1100

    Create IBalancerPool.sol

---
+++ IBalancerPool.sol
@@ -0,0 +1,4 @@
+interface IBalancerInterface {
+    function swapExactAmountIn(address, uint, address, uint, uint) external returns (uint, uint);
+    function swapExactAmountOut(address, uint, address, uint, uint) external returns (uint, uint);
+}
